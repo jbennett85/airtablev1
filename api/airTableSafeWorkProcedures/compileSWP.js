a84f739434bdc3595e4da440dd3db61b00631673
jbennett85 <61759761+jbennett85@users.noreply.github.com>
2023-03-15 11:02:56 +1000

    tests

--- api/airTableSafeWorkProcedures/compileSWP.js
+++ api/airTableSafeWorkProcedures/compileSWP.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
   //console.log(req.body)
   //console.log(swpTemplateURL)
 
-  res.send({response:JSON.stringify(swpTemplateURL)})
+  res.json(req.body)
   // const response = await axios.get(swpTemplateURL, { responseType: 'arraybuffer' })
   // res.send (response.data)
 
